6cf3a12c0db72b48b7480ae377e42fefed5fc392
Robert Hjortsmarker <robert.hjortsmarker@gmail.com>
2024-01-27 12:51:34 +0100

    Updated database migrations

--- src/main/resources/db/migration/V1__baseline.sql
+++ src/main/resources/db/migration/V1__baseline.sql
@@ -1,20 +1,29 @@
 
--- Sound categories
-CREATE TABLE IF NOT EXISTS categories (
-  id VARCHAR(36) PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-
-  CONSTRAINT uc_categories UNIQUE (id, name)
-);
 
 -- Table for storing all sounds
 CREATE TABLE IF NOT EXISTS sounds (
   id VARCHAR(36) PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  categoryId VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  bpm INTEGER NOT NULL,
+  duration_in_seconds INTEGER NOT NULL
+);
 
-  FOREIGN KEY (categoryId) REFERENCES categories(id),
-  CONSTRAINT uc_sounds UNIQUE (id, name, categoryId)
+-- Genres
+CREATE TABLE IF NOT EXISTS genres (
+  id VARCHAR(36) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+
+  CONSTRAINT uc_genres UNIQUE (id, name)
+);
+
+-- Mapping sounds to genres
+CREATE TABLE IF NOT EXISTS sound_genres (
+  sound_id VARCHAR(36) NOT NULL,
+  genre_id VARCHAR(36) NOT NULL,
+
+  FOREIGN KEY (sound_id) REFERENCES sounds(id),
+  FOREIGN KEY (genre_id) REFERENCES genres(id),
+  CONSTRAINT uc_sound_genres UNIQUE (sound_id, genre_id)
 );
 
 -- Table for collecting sounds in playlists
@@ -28,13 +37,21 @@ CREATE TABLE IF NOT EXISTS  playlists (
 
 );
 
--- Tags for explaining the sounds
-CREATE TABLE IF NOT EXISTS sound_tags (
-  sound_id VARCHAR(36) NOT NULL,
-  tag VARCHAR(255) NOT NULL,
+-- Artists
+CREATE TABLE IF NOT EXISTS artists (
+  id VARCHAR(36)  PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
 
-    FOREIGN KEY (sound_id) REFERENCES sounds(id),
-    CONSTRAINT uc_sound_tags UNIQUE (sound_id, tag)
+-- Credits (mapping sounds to artists)
+CREATE TABLE IF NOT EXISTS credits (
+  sound_id VARCHAR(36) NOT NULL,
+  artist_id VARCHAR(36) NOT NULL,
+  role VARCHAR(255) NOT NULL,
+
+  FOREIGN KEY (sound_id) REFERENCES sounds(id),
+  FOREIGN KEY (artist_id) REFERENCES artists(id),
+  CONSTRAINT uc_credits UNIQUE (sound_id, artist_id, role)
 );
 
 
